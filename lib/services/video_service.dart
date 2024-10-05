24179d96afb5991358681a9c8ff27c3e52d88636
Sreekath T <sreekatht@192.168.1.5>
2024-10-05 17:47:57 +0530

    Added more animations

--- lib/services/video_service.dart
+++ lib/services/video_service.dart
@@ -45,10 +45,20 @@ class VideoService {
       final Random random = Random();
 
       final List<String> effects = [
-        "zoompan=z='zoom+0.0015':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d={duration}:s=1080x1920", //zoom in
-        "zoompan=z='zoom+0.005':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=25:s=1080x1920", // zoom in multiple times
-        "zoompan=z=1.5:x='iw/2-(iw/zoom/2)':y='random(1)*20':d={duration}:s=1080x1920", // jerk animation
-        "zoompan=z='1.3+random(1)*0.1':x='random(1)*iw':y='random(1)*ih':d={duration}:s=1080x1920", // Random zoom with both horizontal and vertical random panning
+        //Zoom in
+        "zoompan=z='zoom+0.0015':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d={duration}:s=1080x1920",
+        //Zoom in multiple times
+        "zoompan=z='zoom+0.005':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=25:s=1080x1920",
+        //Jerk animation
+        "zoompan=z=1.5:x='iw/2-(iw/zoom/2)':y='random(1)*20':d={duration}:s=1080x1920",
+        //Random zoom with both horizontal and vertical random panning
+        "zoompan=z='1.3+random(1)*0.1':x='random(1)*iw':y='random(1)*ih':d={duration}:s=1080x1920",
+        //Pan left to right
+        "zoompan=z=1.3:x='(iw-iw/zoom)*(1-on/({duration}*0.7))':y='ih/2-(ih/zoom/2)':d={duration}:s=1080x1920",
+        "zoompan=z=1.4:x='(iw-iw/zoom)*(1-on/({duration}*0.5))':y='ih/2-(ih/zoom/2)':d={duration}:s=1080x1920",
+        //Pan right to left
+        "zoompan=z=1.3:x='(iw-iw/zoom)*(on/({duration}*0.7))':y='ih/2-(ih/zoom/2)':d={duration}:s=1080x1920",
+        "zoompan=z=1.3:x='(iw-iw/zoom)*(on/({duration}*0.5))':y='ih/2-(ih/zoom/2)':d={duration}:s=1080x1920"
       ];
 
       for (var scene in scenes) {
